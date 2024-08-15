d1b252016440c423e13761127fcb4fb430bcac99
kopvi46 <140708770+kopvi46@users.noreply.github.com>
2024-08-15 12:40:30 +0300

    just started

--- Assets/Scripts/Fireplace.cs
+++ Assets/Scripts/Fireplace.cs
@@ -7,6 +7,9 @@ public class Fireplace : MonoBehaviour
     public static Fireplace Instance;
 
     [SerializeField] private int fireplaceHealth;
+
+    private bool isPlayerTriggered = false;
+
     public int FireplaceHealth 
     { 
         get 
@@ -28,5 +31,33 @@ public class Fireplace : MonoBehaviour
     private void Awake()
     {
         Instance = this;
+    }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.TryGetComponent<Player>(out Player player))
+    //    {
+    //        Debug.Log("Player near fireplace!");
+    //    }
+    //}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<Player>(out Player player))
+        {
+            isPlayerTriggered = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.TryGetComponent<Player>(out Player player))
+        {
+            isPlayerTriggered = false;
+        }
+    }
+
+    public bool GetIsPlayerTriggered()
+    {
+        return isPlayerTriggered;
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,7 +9,11 @@ public class Player : MonoBehaviour
     [SerializeField] private float moveSpeed;
     [SerializeField] private GameInput gameInput;
 
-    [SerializeField] private int playerHealth = 100;
+    private int playerHealth = 100;
+    private float healthChangeDelay = 3f;
+    private float healtChangeTimer = 0f;
+
+
     public int PlayerHealth 
     {  
         get 
@@ -37,6 +41,24 @@ public class Player : MonoBehaviour
     private void Update()
     {
         HandleMovement();
+
+        healtChangeTimer -= Time.deltaTime;
+
+        if (Fireplace.Instance.GetIsPlayerTriggered())
+        {
+            if (healtChangeTimer < 0)
+            {
+                healtChangeTimer = healthChangeDelay;
+                playerHealth += 10;
+            }
+        } else
+        {
+            if (healtChangeTimer < 0)
+            {
+                healtChangeTimer = healthChangeDelay;
+                playerHealth -= 10;
+            }
+        }
     }
 
     private void HandleMovement()
@@ -56,7 +78,47 @@ public class Player : MonoBehaviour
 
         float moveDistance = moveSpeed * Time.deltaTime;
 
-        transform.position += directionVector * moveDistance;
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, directionVector, moveDistance);
+
+        if (!canMove)
+        {
+            //Cannot move towards directionVector
+
+            //Atempt only x movement
+            Vector3 directionVectorX = new Vector3(directionVector.x, 0, 0).normalized;
+            canMove = (directionVector.x < -.5f || directionVector.x > .5f) && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, directionVectorX, moveDistance);
+
+            if (canMove)
+            {
+                //Can move only on the X
+                directionVector = directionVectorX;
+            } else
+            {
+                //Cannot move only on the X
+
+                //Attempt only Z movement
+                Vector3 directionVectorZ = new Vector3(0, 0, directionVector.z).normalized;
+                canMove = (directionVector.z < -.5f || directionVector.z > .5f) && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, directionVectorZ, moveDistance);
+
+                if (canMove)
+                {
+                    //Can move only on the Z
+                    directionVector = directionVectorZ;
+                } else
+                {
+                    //Cannot move in any direction
+                }
+            }
+        }
+
+        if (canMove)
+        {
+            transform.position += directionVector * moveDistance;
+        }
+
+        //transform.position += directionVector * moveDistance;
 
         if (directionVector != Vector3.zero)
         {
